083726ac7b06b4d9dbe8af3c21ff822acd123fc6
Esteban Cairol <ecairol@gmail.com>
2022-08-27 13:38:07 -0600

    Add one more link to list. Add slight animation to hover links

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 
-<link rel="stylesheet" href="src/styles.css?v1">
+<link rel="stylesheet" href="src/styles.css?v2">
 
 <article>
 	<h1>Welcome!</h1>
@@ -12,5 +12,6 @@
 		<li><a target="_blank" href="https://opensource.com/article/20/4/git-rebase-i">The life-changing magic of git rebase -i</a></li>
 		<li><a target="_blank" href="https://opensource.com/article/20/7/git-best-practices">6 best practices for teams using Git</a></li>
 		<li><a target="_blank" href="https://www.honeybadger.io/blog/git-tricks/">Top Ten Git Tips & Tricks</a></li>
+		<li><a target="_blank" href="https://codingsight.com/git-branching-naming-convention-best-practices/">Git Branching Naming Convention: Best Practices</a></li>
 	</ul>
 </article>
