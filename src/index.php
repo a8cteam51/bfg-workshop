ec5f19fd09e39a6d2345c6b055fac48baf3da43a
Esteban Cairol <ecairol@gmail.com>
2022-08-27 13:28:59 -0600

    Open links in new tab

--- src/index.php
+++ src/index.php
@@ -8,9 +8,9 @@
 	<p>Here are some useful resources to make the most out of Git:</p>
 
 	<ul>
-		<li><a href="https://opensource.com/article/20/4/git-merge-conflict">How to resolve a git merge conflict</a></li>
-		<li><a href="https://opensource.com/article/20/4/git-rebase-i">The life-changing magic of git rebase -i</a></li>
-		<li><a href="https://opensource.com/article/20/7/git-best-practices">6 best practices for teams using Git</a></li>
-		<li><a href="https://www.honeybadger.io/blog/git-tricks/">Top Ten Git Tips & Tricks</a></li>
+		<li><a target="_blank" href="https://opensource.com/article/20/4/git-merge-conflict">How to resolve a git merge conflict</a></li>
+		<li><a target="_blank" href="https://opensource.com/article/20/4/git-rebase-i">The life-changing magic of git rebase -i</a></li>
+		<li><a target="_blank" href="https://opensource.com/article/20/7/git-best-practices">6 best practices for teams using Git</a></li>
+		<li><a target="_blank" href="https://www.honeybadger.io/blog/git-tricks/">Top Ten Git Tips & Tricks</a></li>
 	</ul>
 </article>
